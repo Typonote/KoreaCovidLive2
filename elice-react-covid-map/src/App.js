0d1a847eb32782911aef1ed7bf5fec3394a0c2a5
[2기-1반] 김진경 <kimradium@naver.com>
2021-08-11 13:49:38 +0900

    #3 yarn add redux와 yarn add react-redux 실시 후 Provider삽입

--- elice-react-covid-map/src/App.js
+++ elice-react-covid-map/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import CovidMap from './CovidMap';
+import {Provider} from "react-redux";
+import store from "./redux/store";
+
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
         <CovidMap />
-    </div>
+    </Provider>
   );
 }
 
--- elice-react-covid-map/src/redux/action.js
+++ elice-react-covid-map/src/redux/action.js
@@ -1,4 +1,6 @@
-const changeTheme = (theme) => ({
+export const changeTheme = (theme) => ({
     type: "CHANGE_THEME",
     theme: theme
 })
+
+// ({}) 이런식으로 하면 객체를 바로 return
